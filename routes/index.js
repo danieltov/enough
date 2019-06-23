01514ba1e1037539a600d132928afc7827ffdd4b
Daniel Tovar <daniel@danieltovar.io>
2019-06-23 16:40:55 -0600

    Cleaned up User API Route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const router = require('express').Router();
 router.use('/api/users', require('./api/users'));
 router.use('/api/auth', require('./api/auth'));
 
+// * Test
+router.get('/', (req, res) => res.send('API Running'));
+
 // If no API routes are hit, send the React app
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
