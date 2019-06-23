1a5035e74dc5029d4e0b7ae68dab7dbf61e672d3
Daniel Tovar <daniel@danieltovar.io>
2019-06-23 11:46:28 -0600

    Created User Model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  affirmations: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'achievements'
+    },
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'favoriteQuotes'
+    },
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'gratitudes'
+    },
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'strengths'
+    }
+  ],
+  favoriteAnimal: {
+    type: String
+  }
+});
+module.exports = User = mongoose.model('user', UserSchema, 'users');
