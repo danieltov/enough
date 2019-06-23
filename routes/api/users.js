bc587fc83afc0da4170c30ffa268c39e4149ea21
Daniel Tovar <daniel@danieltovar.io>
2019-06-23 12:12:23 -0600

    POST register user route created

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,80 @@
+// * ==================== DEPENDENCIES ==================== *//
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('dotenv').config();
+const { check, validationResult } = require('express-validator/check'); // ! use express-validate to handle validation and responses
+const User = require('../../models/User');
 
-// * @route   GET api/users
-// * @desc    Test route
+// * ==================== ROUTES ==================== *//
+
+// * @route   POST api/users
+// * @desc    Register user
 // * @access  Public
-router.get('/', (req, res) => res.send('Messages route'));
+
+router.post(
+  '/',
+  [
+    // ! express-validate functions to validate request body START
+    check('name', 'Please enter a name to call you by')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 8 or more characters'
+    ).isLength({ min: 8 })
+    // ! express-validate functions to validate request body END
+  ],
+  async (req, res) => {
+    // ! express-validate error catching START
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // ! express-validate error catching END
+
+    const { name, email, password } = req.body;
+
+    try {
+      // * See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // * Create an instance of the user
+      user = new User({ name, email, password });
+
+      // * Encrypt password with bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // * Save user to database
+      await user.save();
+
+      // * Return jsonwebtoken
+      const payload = { user: { id: user.id } };
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
