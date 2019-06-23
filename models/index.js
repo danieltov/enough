8bb507cc533b0415368a287ed607f7295a8544dc
Daniel Tovar <daniel@danieltovar.io>
2019-06-23 15:00:40 -0600

    Indexed models for easier routing

---
+++ models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  User: require('./User'),
+  Affirmation: require('./BaseAffirmation'),
+  Gratitude: require('./Gratitude'),
+  Quote: require('./Quote'),
+  Strength: require('./Strength'),
+  Achievement: require('./Achievement')
+};
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.use('/api/users', require('./api/users'));
 router.use('/api/auth', require('./api/auth'));
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
+router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
