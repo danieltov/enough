4754c402e29d82e275c2f641e475a26a229f13c3
Daniel Tovar <daniel@danieltovar.io>
2019-06-23 11:55:40 -0600

    Created BaseAffirmation Model

---
+++ models/BaseAffirmation.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const baseOptions = {
+  discriminatorKey: 'affirmationType',
+  collection: 'affirmations'
+};
+
+const BaseAffirmationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user'
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String
+    },
+    dateAdded: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  baseOptions
+);
+
+module.exports = Affirmation = mongoose.model(
+  'affirmation',
+  BaseAffirmationSchema
+);
