eda1a5341697f3b08b350107961f1b6aa2e195dd
Daniel Tovar <daniel@danieltovar.io>
2019-06-24 15:28:07 -0600

    Created affController and updated Aff Routes

--- routes/api/aff.js
+++ routes/api/aff.js
@@ -1,13 +1,11 @@
 // * ==================== DEPENDENCIES ==================== *//
 const express = require('express');
 const config = require('dotenv').config();
-const { validationResult, body } = require('express-validator/check');
+const { body } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const DB = require('../../models');
 const auth = require('../../middleware/auth');
+const controller = require('../../controllers/affController');
 
 // * ==================== ROUTES ==================== *//
 
@@ -19,8 +17,8 @@ router.post(
   '/',
   [
     auth,
+    // * Validate request
     [
-      // ! express-validate functions to validate request body START
       body('text', 'Text is required')
         .not()
         .isEmpty()
@@ -29,66 +27,9 @@ router.post(
       body('affirmationType', 'An Affirmation Type is required')
         .not()
         .isEmpty()
-      // ! express-validate functions to validate request body END
     ]
   ],
-  async (req, res) => {
-    // ! express-validate error catching START
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array()
-      });
-    }
-    // ! express-validate error catching END
-
-    // * Destructure req.body
-    const { text, image, dateAdded, author, affirmationType } = req.body;
-
-    // * Build affirmation object
-    const affirmationFields = {};
-    affirmationFields.user = req.user.id;
-    affirmationFields.text = text;
-    affirmationFields.affirmationType = affirmationType;
-    if (image) affirmationFields.image = image;
-    if (dateAdded) affirmationFields.dateAdded = dateAdded;
-    if (author) affirmationFields.author = author;
-
-    // * Add affirmationFields to corresponding model
-    try {
-      if (affirmationFields.affirmationType === 'gratitude') {
-        const gratitude = new DB.Gratitude(affirmationFields);
-        await gratitude.save();
-        const user = await DB.User.findOneAndUpdate(
-          { _id: affirmationFields.user },
-          { $push: { 'affirmations.gratitudes': gratitude._id } },
-          { new: true }
-        );
-        return res.json({ gratitude, user });
-      } else if (affirmationFields.affirmationType === 'quote') {
-        const quote = new DB.Quote(affirmationFields);
-        await quote.save();
-        const user = await DB.User.findOneAndUpdate(
-          { _id: affirmationFields.user },
-          { $push: { 'affirmations.quotes': quote._id } },
-          { new: true }
-        );
-        return res.json({ quote, user });
-      } else {
-        const strength = new DB.Strength(affirmationFields);
-        await strength.save();
-        const user = await DB.User.findOneAndUpdate(
-          { _id: affirmationFields.user },
-          { $push: { 'affirmations.strengths': strength._id } },
-          { new: true }
-        );
-        return res.json({ strength, user });
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  }
+  controller.newAff
 );
 
 // * @route   POST api/aff/achievement
@@ -99,8 +40,8 @@ router.post(
   '/achievement',
   [
     auth,
+    // * Validate request
     [
-      // ! express-validate functions to validate request body START
       body('text', 'Text is required')
         .not()
         .isEmpty()
@@ -126,63 +67,9 @@ router.post(
         .isEmpty()
         .trim()
         .escape()
-      // ! express-validate functions to validate request body END
     ]
   ],
-  async (req, res) => {
-    // ! express-validate error catching START
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array()
-      });
-    }
-    // ! express-validate error catching END
-
-    // * Destructure req.body
-    const {
-      text,
-      image,
-      dateAdded,
-      affirmationType,
-      title,
-      dateAchieved,
-      madeMeFeel
-    } = req.body;
-
-    // * Build affirmation object
-    const affirmationFields = {};
-    affirmationFields.user = req.user.id;
-    affirmationFields.text = text;
-    affirmationFields.affirmationType = affirmationType;
-    affirmationFields.dateAdded = dateAdded;
-    affirmationFields.title = title;
-    affirmationFields.dateAchieved = dateAchieved;
-    affirmationFields.madeMeFeel = madeMeFeel;
-    if (image) affirmationFields.image = image;
-
-    // * Add affirmationFields to model
-    try {
-      // * make sure dateAchieved isn't null
-      if (affirmationFields.dateAchieved === null) {
-        return res.status(400).json({
-          msg: 'Accomplishment Achieved Date is inavlid'
-        });
-      }
-
-      const achievement = new DB.Achievement(affirmationFields);
-      await achievement.save();
-      const user = await DB.User.findOneAndUpdate(
-        { _id: affirmationFields.user },
-        { $push: { 'affirmations.achievements': achievement._id } },
-        { new: true }
-      );
-      return res.json({ achievement, user });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  }
+  controller.achievment
 );
 
 module.exports = router;
