fb7eb6c1fc93814c61edd2248ace47a37c28512c
Daniel Tovar <daniel@danieltovar.io>
2019-06-24 14:44:59 -0600

    Created User Controller and tested on Routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,9 @@
 // * ==================== DEPENDENCIES ==================== *//
 const express = require('express');
 const config = require('dotenv').config();
-const { validationResult, body } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
+const { body } = require('express-validator/check');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const DB = require('../../models');
-const auth = require('../../middleware/auth');
+const controller = require('../../controllers/userController');
 
 // * ==================== ROUTES ==================== *//
 
@@ -33,53 +29,7 @@ router.post(
     ).isLength({ min: 8 })
     // ! express-validate functions to validate request body END
   ],
-  async (req, res) => {
-    // ! express-validate error catching START
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    // ! express-validate error catching END
-
-    const { name, email, password } = req.body;
-
-    try {
-      // * See if user exists
-      let user = await DB.User.findOne({ email });
-
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
-      }
-
-      // * Create an instance of the user
-      user = new DB.User({ name, email, password });
-
-      // * Encrypt password with bcrypt
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
-
-      // * Save user to database
-      await user.save();
-
-      // * Return jsonwebtoken
-      const payload = { user: { id: user.id } };
-      jwt.sign(
-        payload,
-        process.env.JWT_SECRET,
-        { expiresIn: 3600 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
-  }
+  controller.register
 );
 
 module.exports = router;
