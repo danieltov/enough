cd69343090a2f9eb9d44eb7f5d0efdb61e8f2f5f
Daniel Tovar <daniel@danieltovar.io>
2019-06-27 19:45:11 -0600

    Implemented React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Wrapper from './components/Wrapper';
+import { EnoughNav as Nav } from './components/Navbar';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Landing />
-  </Fragment>
+  <Router>
+    <Wrapper>
+      <Nav />
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+      </Switch>
+    </Wrapper>
+  </Router>
 );
 
 export default App;
