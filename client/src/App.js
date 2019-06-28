5701630c29a53eab3df7c63cd061cccd3c1fa761
Daniel Tovar <daniel@danieltovar.io>
2019-06-28 12:08:40 -0600

    Organized React Router switch paths on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,28 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Wrapper from './components/Wrapper';
 import { EnoughNav as Nav } from './components/Navbar';
+import Container from 'react-bootstrap/Container';
 import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
 import Dashboard from './pages/Dashboard';
 import './App.css';
 
 const App = () => (
   <Router>
-    <Wrapper>
-      <Nav />
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/dashboard' component={Dashboard} />
-      </Switch>
-    </Wrapper>
+    <Fragment>
+      <Wrapper>
+        <Nav />
+        <Switch>
+          <Container className='d-flex flex-column align-content-center hero'>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </Container>
+        </Switch>
+      </Wrapper>
+    </Fragment>
   </Router>
 );
 
