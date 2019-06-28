d849196a39626aa8ecbbfacfdd7b69fc858caaf3
Daniel Tovar <daniel@danieltovar.io>
2019-06-28 16:26:47 -0600

    Implemented Redux to App.js + better comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,41 @@
+// * ==================== REACT COMPONENTS ==================== *//
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Wrapper from './components/Wrapper';
 import { EnoughNav as Nav } from './components/Navbar';
 import Container from 'react-bootstrap/Container';
+
+// * ==================== VIEWS ==================== *//
 import Landing from './pages/Landing';
 import Login from './pages/auth/Login';
 import Signup from './pages/auth/Signup';
 import Dashboard from './pages/Dashboard';
+
+// * ==================== REDUX ==================== *//
+import { Provider } from 'react-redux';
+import store from './store';
+
+// * ==================== STYLES ==================== *//
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Wrapper>
-        <Nav />
-        <Switch>
-          <Container className='d-flex flex-column align-content-center hero'>
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/dashboard' component={Dashboard} />
-          </Container>
-        </Switch>
-      </Wrapper>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Wrapper>
+          <Nav />
+          <Switch>
+            <Container className='d-flex flex-column align-content-center hero'>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/' component={Landing} />
+              <Route exact path='/dashboard' component={Dashboard} />
+            </Container>
+          </Switch>
+        </Wrapper>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
