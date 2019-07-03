6660b92b4b460790b017cd477ace1e50e2cb8792
Daniel Tovar <daniel@danieltovar.io>
2019-07-02 18:25:11 -0600

    Created new types for login actions

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,5 @@ export const SET_NOTICE = 'SET_NOTICE';
 export const REMOVE_NOTICE = 'REMOVE_NOTICE';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
