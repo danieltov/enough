dfffa8a1ec5e1b41ca9bb57f38d76db1c9a17dc6
Daniel Tovar <daniel@danieltovar.io>
2019-07-04 14:13:34 -0600

    Implemented userLoaded action/reducers to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // * ==================== REACT COMPONENTS ==================== *//
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Wrapper from './components/Wrapper';
 import { EnoughNav as Nav } from './components/Navbar';
@@ -15,27 +15,43 @@ import Aff from './pages/aff/Achievement';
 // * ==================== REDUX ==================== *//
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 // * ==================== STYLES ==================== *//
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Wrapper>
-        <Nav />
-        <Container className='d-flex flex-column align-content-center hero'>
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/aff' component={Aff} />
-          </Switch>
-        </Container>
-      </Wrapper>
-    </Router>
-  </Provider>
-);
+// * ==================== LOGIC ==================== *//
+
+// * Check to see if there's a token in localStorage
+if (localStorage.token) {
+  // * If token, set global header
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Wrapper>
+          <Nav />
+          <Container className='d-flex flex-column align-content-center hero'>
+            <Switch>
+              <Route exact path='/' component={Landing} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/dashboard' component={Dashboard} />
+              <Route exact path='/aff' component={Aff} />
+            </Switch>
+          </Container>
+        </Wrapper>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
