18682e003197e10bab243c220e78050ad1b9fe19
Daniel Tovar <daniel@danieltovar.io>
2019-07-05 15:33:17 -0600

    Created login success/fail constant types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,5 +2,7 @@ export const SET_NOTICE = 'SET_NOTICE';
 export const REMOVE_NOTICE = 'REMOVE_NOTICE';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
