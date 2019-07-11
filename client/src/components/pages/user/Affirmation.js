45f37bc337ec93d267568796c80311a5ddf08ab6
Daniel Tovar <daniel@danieltovar.io>
2019-07-10 19:57:53 -0600

    Created render method for strength and gratitude

--- client/src/components/pages/user/Affirmation.js
+++ client/src/components/pages/user/Affirmation.js
@@ -2,20 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-// import { Row, Col } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import { Row, Col, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Affirmation = ({
   affirmations: { achievements, quotes, strengths, gratitudes }
 }) => {
-  // TODO push all affirmations into a master array
   let masterArray = [];
   if (achievements.length) achievements.forEach(a => masterArray.push(a));
   if (quotes.length) quotes.forEach(q => masterArray.push(q));
   if (strengths.length) strengths.forEach(s => masterArray.push(s));
   if (gratitudes.length) gratitudes.forEach(g => masterArray.push(g));
 
-  // TODO Shuffle array
   const shuffler = array => {
     let currentIndex = array.length;
     let temporaryValue, randomIndex;
@@ -34,7 +32,51 @@ const Affirmation = ({
 
   // TODO Populate component with first element from array
 
-  return <div>{JSON.stringify(masterArray[0])}</div>;
+  // * Destructure affirmation object
+  const {
+    text,
+    image,
+    affirmationType,
+    title,
+    dateAchieved,
+    madeMeFeel
+  } = masterArray[0];
+
+  console.log(affirmationType);
+
+  const strOrGrat = (affirmationType, text, image) => (
+    <>
+      <Card.Title>
+        {affirmationType === 'strength' ? (
+          <h2 style={{ textAlign: 'center', color: '#292929' }}>
+            You may not feel it now but remember, you are{' '}
+            <strong>strong</strong> enough.
+          </h2>
+        ) : (
+          <h2>
+            It may not feel like it now, but try to remember what you're
+            <strong>grateful</strong> for.
+          </h2>
+        )}
+      </Card.Title>
+      <Card.Text style={{ color: '#292929' }}>
+        <em>On a better day, you asked us to remind you:</em> "{text}"
+      </Card.Text>
+    </>
+  );
+
+  return (
+    <>
+      <Card body>
+        {affirmationType !== 'achievement' && affirmationType !== 'quote'
+          ? strOrGrat(affirmationType, text, image)
+          : affirmationType !== 'achievement'
+          ? 'quote'
+          : 'achievement'}
+      </Card>
+      <>{image && <Card.Img variant='bottom' />}</>
+    </>
+  );
 };
 
 Affirmation.propTypes = {
--- models/BaseAffirmation.js
+++ models/BaseAffirmation.js
@@ -7,10 +7,6 @@ const baseOptions = {
 
 const BaseAffirmationSchema = new mongoose.Schema(
   {
-    // user: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: 'user'
-    // },
     text: {
       type: String,
       required: true
