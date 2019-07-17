ad6c503f2d632e46acfb4f4b8720c7f6c7c0f863
Daniel Tovar <daniel@danieltovar.io>
2019-07-16 23:32:57 -0600

    build commit

---
+++ client/build/precache-manifest.93276279dc7ef738f9142d1aa426ca9e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fb3018adb71b1129b70d56e7b13f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1cb56fcb63b3b7b1766",
+    "url": "/static/css/main.1421ad94.chunk.css"
+  },
+  {
+    "revision": "1134fa8e45326fb5e2c2",
+    "url": "/static/js/0.32ecbad7.chunk.js"
+  },
+  {
+    "revision": "7f8b0d4b441728ab35a3",
+    "url": "/static/js/10.1cc2101d.chunk.js"
+  },
+  {
+    "revision": "9aca0dfb8e506e1fe0c5",
+    "url": "/static/js/3.6f7e1cc6.chunk.js"
+  },
+  {
+    "revision": "a15989830c239f2117fd",
+    "url": "/static/js/4.775fde30.chunk.js"
+  },
+  {
+    "revision": "bda5f1622dc4813863ab",
+    "url": "/static/js/5.6e91adf8.chunk.js"
+  },
+  {
+    "revision": "007c40f221566492766f",
+    "url": "/static/js/6.270e63e5.chunk.js"
+  },
+  {
+    "revision": "ef49c05f3090c3da16eb",
+    "url": "/static/js/7.e3936caa.chunk.js"
+  },
+  {
+    "revision": "df7c384ffe2771de9a99",
+    "url": "/static/js/8.30b9016f.chunk.js"
+  },
+  {
+    "revision": "ded55b68e54541f90c4d",
+    "url": "/static/js/9.02766df3.chunk.js"
+  },
+  {
+    "revision": "f1cb56fcb63b3b7b1766",
+    "url": "/static/js/main.c2b23a5c.chunk.js"
+  },
+  {
+    "revision": "ddff10daa15204c89811",
+    "url": "/static/js/runtime~main.4e4db281.js"
+  }
+]);
