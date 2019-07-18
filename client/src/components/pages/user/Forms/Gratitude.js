eb7318db4df69d0c04eebc302a0222911b54039a
Daniel Tovar <daniel@danieltovar.io>
2019-07-17 20:26:58 -0600

    Made gratitude route more descriptive and fixed DOM warnings

--- client/src/components/pages/user/Forms/Gratitude.js
+++ client/src/components/pages/user/Forms/Gratitude.js
@@ -12,24 +12,10 @@ import Notice from '../../../Notice';
 const Gratitude = ({ newAffirmation, setNotice, count, history }) => {
   const [formData, setFormData] = useState({
     text: '',
-    image: '',
-    dateAdded: '',
-    affirmationType: 'gratitude',
-    title: '',
-    dateAchieved: '',
-    madeMeFeel: ''
+    affirmationType: 'gratitude'
   });
 
-  const {
-    text,
-    image,
-    dateAdded,
-    affirmationType,
-    title,
-    dateAchieved,
-    madeMeFeel,
-    author
-  } = formData;
+  const { text } = formData;
 
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -40,29 +26,27 @@ const Gratitude = ({ newAffirmation, setNotice, count, history }) => {
   };
 
   return (
-    <Row>
-      <Col xs={12} className='d-flex justify-content-center align-items-center'>
+    <Row className='d-flex justify-content-center align-items-center'>
+      <Col
+        xs={12}
+        md={8}
+        className='d-flex justify-content-center align-items-center'>
         <div className='form-container'>
-          <Form onSubmit={e => onSubmit(e)}>
+          <Form id='affForm' onSubmit={e => onSubmit(e)}>
             <h2 className='text-center'>
               {!count ? 'Enter your first affirmation! ' : ''}What is one thing
               you're <strong>grateful</strong> for?
             </h2>
             <Notice />
             <Form.Group>
-              <Form.Control
-                type='text'
-                name='achievementType'
-                placeholder='Gratitude'
-                value={'gratitude'}
-                readOnly
-              />
-            </Form.Group>
-            <Form.Group>
+              <Form.Label htmlFor='text'>
+                Enter something you're grateful for:
+              </Form.Label>
               <Form.Control
                 type='text'
                 name='text'
-                placeholder='Gratitude'
+                id='gratitudeText'
+                placeholder='Example: My dog.'
                 onChange={e => onChange(e)}
                 value={text}
               />
