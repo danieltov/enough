2180d268f50b1e3406b6916b3d30fa8217d07786
Daniel Tovar <daniel@danieltovar.io>
2019-07-18 22:39:00 -0600

    new build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93276279dc7ef738f9142d1aa426ca9e.js"
+  "/precache-manifest.82405ed7bec12ff4c98cb68c19c2485e.js"
 );
 
 self.addEventListener('message', (event) => {
