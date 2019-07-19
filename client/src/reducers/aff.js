aeb8aa1e46b2b9f6956bc558c99aed3ce68b7c90
Daniel Tovar <daniel@danieltovar.io>
2019-07-18 21:49:56 -0600

    created delete aff redux reducer

--- client/src/reducers/aff.js
+++ client/src/reducers/aff.js
@@ -1,4 +1,9 @@
-import { ADD_AFF_SUCCESS, ADD_AFF_FAIL } from '../actions/types';
+import {
+  ADD_AFF_SUCCESS,
+  ADD_AFF_FAIL,
+  DELETE_AFF_SUCCESS,
+  DELETE_AFF_FAIL
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -10,8 +15,10 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case ADD_AFF_SUCCESS:
+    case DELETE_AFF_SUCCESS:
       return { ...state, data: payload, success: true };
     case ADD_AFF_FAIL:
+    case DELETE_AFF_FAIL:
       return { ...state, success: false };
     default:
       return state;
