0e52b79c564e2c4daa43629d620c76b2d7a964b3
Daniel Tovar <daniel@danieltovar.io>
2019-07-20 20:37:44 -0600

    Connected POST Mood redux action/reducer to Dashboard

--- client/src/components/pages/user/Dashboard.js
+++ client/src/components/pages/user/Dashboard.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import { setNotice } from '../../../actions/notice';
+import { postMood } from '../../../actions/mood';
 
 import { Row, Col } from 'react-bootstrap';
 import { Link, Redirect } from 'react-router-dom';
 import Notice from '../../Notice';
 
-const Dashboard = ({ count, setNotice }) => {
+const Dashboard = ({ count, setNotice, postMood }) => {
   // * Redirect affirmation count is 0
   if (!count) return <Redirect to='/affirm' />;
 
@@ -21,21 +22,21 @@ const Dashboard = ({ count, setNotice }) => {
       <Col xs={12}>
         <Row className='m-3 mt-5'>
           <Col xs={12} md={4}>
-            <Link to='/affirm'>
+            <Link to='/affirm' onClick={() => postMood({ value: 1 })}>
               <h1 className='text-center mt-0 '>
                 <i className='fas fa-laugh-beam dash-icon' />
               </h1>
             </Link>
           </Col>
           <Col xs={12} md={4}>
-            <Link to='/affirmation'>
+            <Link to='/affirmation' onClick={() => postMood({ value: 0 })}>
               <h1 className='text-center mt-0 '>
                 <i className='fas fa-meh dash-icon' />
               </h1>
             </Link>
           </Col>
           <Col xs={12} md={4}>
-            <Link to='/affirmation'>
+            <Link to='/affirmation' onClick={() => postMood({ value: -1 })}>
               <h1 className='text-center mt-0 '>
                 <i className='fas fa-frown dash-icon' />
               </h1>
@@ -49,7 +50,8 @@ const Dashboard = ({ count, setNotice }) => {
 
 Dashboard.propTypes = {
   count: PropTypes.number.isRequired,
-  setNotice: PropTypes.func.isRequired
+  setNotice: PropTypes.func.isRequired,
+  postMood: PropTypes.func.isRequired
 };
 
 // ! Get the information from state that we need from our reducer
@@ -61,5 +63,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { setNotice }
+  { setNotice, postMood }
 )(Dashboard);
